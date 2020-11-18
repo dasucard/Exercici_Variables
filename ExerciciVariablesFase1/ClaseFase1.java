5a4ca3352a468d605d99b490b1f37c653170e461
dasucard <dasucard@gmail.com>
2020-11-18 16:44:23 +0100

    Primer Ejercicio de Variables Fase 1

---
+++ ExerciciVariablesFase1/ClaseFase1.java
@@ -0,0 +1,21 @@
+
+public class ClaseFase1 {
+
+	public static void main(String[] args) {
+		
+		String Nom="David";
+		String Cognom1="Suárez";
+		String Cognom2="Cárdenas";
+		
+		System.out.println(Cognom1+Cognom2+Nom);
+		
+		
+		int dia=18;
+		int mes=11;
+		int any=2020;
+		
+		System.out.println(dia+"/"+mes+"/"+any);
+
+	}
+
+}
