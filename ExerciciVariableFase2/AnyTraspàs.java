995ff7220b5cf36c33b8e53772ab42e3d3766f91
dasucard <dasucard@gmail.com>
2020-11-18 17:48:16 +0100

    Exercici1 Fase2

---
+++ ExerciciVariableFase2/AnyTraspàs.java
@@ -0,0 +1,15 @@
+
+public class AnyTraspàs {
+
+	public static void main(String[] args) {
+
+		int a=1984;
+		int Traspàs=4;
+		int Naixement=1994;
+		float count=(Naixement-a)/Traspàs;
+		
+		System.out.println(count);
+		
+	}
+
+}
